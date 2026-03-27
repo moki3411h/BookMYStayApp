3de2a7915985bec9869c5b15640b16b655dfad4e
mok3411h <mokesh7319@gmail.com>
2026-03-27 16:08:50 +0530

    Use Case 4: Room Search & Availability Check

--- BookMyStayApp/src/BookMyStayApp.java
+++ BookMyStayApp/src/BookMyStayApp.java
@@ -2,42 +2,98 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * UseCase3InventorySetup
- * Centralized Room Inventory Management using HashMap
+ * UseCase4RoomSearch
+ * Room Search & Availability Check (Read Only)
  *
- * @version 3.1
+ * @version 4.0
  */
 
-// Inventory Class
+// Abstract Room
+abstract class Room {
+    protected String type;
+    protected int beds;
+    protected double price;
+
+    public Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Room Type : " + type);
+        System.out.println("Beds      : " + beds);
+        System.out.println("Price     : ₹" + price);
+    }
+
+    public String getType() {
+        return type;
+    }
+}
+
+// Room Types
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super("Single Room", 1, 2000);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("Double Room", 2, 3500);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super("Suite Room", 3, 6000);
+    }
+}
+
+// Inventory (Read Only Access)
 class RoomInventory {
 
-    private Map<String, Integer> roomAvailability;
+    private Map<String, Integer> availability;
 
-    // Constructor
     public RoomInventory() {
-        roomAvailability = new HashMap<>();
-
-        roomAvailability.put("Single Room", 5);
-        roomAvailability.put("Double Room", 3);
-        roomAvailability.put("Suite Room", 2);
+        availability = new HashMap<>();
+        availability.put("Single Room", 5);
+        availability.put("Double Room", 3);
+        availability.put("Suite Room", 0);
     }
 
-    // Get availability
     public int getAvailability(String roomType) {
-        return roomAvailability.getOrDefault(roomType, 0);
+        return availability.getOrDefault(roomType, 0);
+    }
+}
+
+// Search Service
+class RoomSearchService {
+
+    private RoomInventory inventory;
+
+    public RoomSearchService(RoomInventory inventory) {
+        this.inventory = inventory;
     }
 
-    // Update availability
-    public void updateAvailability(String roomType, int count) {
-        roomAvailability.put(roomType, count);
-    }
+    public void searchRooms() {
 
-    // Display inventory
-    public void displayInventory() {
-        System.out.println("Room Inventory:");
+        Room[] rooms = {
+                new SingleRoom(),
+                new DoubleRoom(),
+                new SuiteRoom()
+        };
 
-        for (Map.Entry<String, Integer> entry : roomAvailability.entrySet()) {
-            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        System.out.println("Available Rooms:\n");
+
+        for (Room room : rooms) {
+            int available = inventory.getAvailability(room.getType());
+
+            if (available > 0) {
+                room.displayDetails();
+                System.out.println("Available : " + available);
+                System.out.println();
+            }
         }
     }
 }
@@ -47,18 +103,13 @@ public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Book My Stay - Hotel Booking System v3.1\n");
+        System.out.println("Book My Stay - Hotel Booking System v4.0\n");
 
         RoomInventory inventory = new RoomInventory();
 
-        inventory.displayInventory();
+        RoomSearchService searchService =
+                new RoomSearchService(inventory);
 
-        System.out.println("\nSingle Room Available: "
-                + inventory.getAvailability("Single Room"));
-
-        inventory.updateAvailability("Suite Room", 1);
-
-        System.out.println("\nUpdated Inventory:");
-        inventory.displayInventory();
+        searchService.searchRooms();
     }
 }
